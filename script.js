85b14dfa0c2f892ba91a405c1daed2c3fa84eb47
Chrisolivieri <christian.olivieri93@gmail.com>
2024-06-05 19:59:02 +0200

    set cart

--- script.js
+++ script.js
@@ -9,11 +9,16 @@ fetch("https://striveschool-api.herokuapp.com/books")
         let allBooks = ""
 
         books.forEach((item) => {
+          
+          let stringaJSON = JSON.stringify(item)
+          let nuovaStringa = stringaJSON.replace(/'/g, "");
+          
+         
           allBooks += ` <div class="card" id = ` + item.asin + ` style="width: 18rem;">
                 <img src= ${item.img} class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">${item.title}</h5>
-                  <button class= "btn btn-warning" onclick='aggiungiAlCarrello(${JSON.stringify(item)})'><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg> Aggiungi al carrello</button>
+                  <button class= "btn btn-warning" onclick='aggiungiAlCarrello(${nuovaStringa})'><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg> Aggiungi al carrello</button>
                   <button class= "btn btn-danger" onclick="nascondiLibro(\`` + item.asin + `\`)">Nascondi libro</button>
                   <a href="./details.html?id=${item.asin}&img=${item.img}&title=${item.title}&ctg=${item.category}&price=${item.price}"> Vedi dettagli</a>
                 </div>
@@ -71,6 +76,8 @@ let carrello = []
 let libriNelCarrello = 0
 
 function aggiungiAlCarrello(libro) {
+  console.log(libro)
+ libro.title = libro.title.replace(/'/g, "\\'");
   carrello.push(libro)
   alert("Libro aggiunto al carrello")
   libriNelCarrello++
